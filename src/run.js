0a38b5e7591fa0fc95ac7c9f23ee8f5328e203f2
Rene Rubalcava <odoe@odoe.net>
2015-02-08 20:19:18 -0800

    party has started

---
+++ src/run.js
@@ -0,0 +1,24 @@
+(function() {
+  'use strict';
+
+  var pathRX = new RegExp(/\/[^\/]+$/)
+    , locationPath = location.pathname.replace(pathRX, '');
+
+  require({
+    async: true,
+    parseOnLoad: true,
+    packages: [{
+      name: 'widgets',
+      location: locationPath + 'dist/widgets'
+    }, {
+      name: 'react',
+      location: locationPath + 'bower_components/react/',
+      main: 'react'
+    }, {
+      name: 'app',
+      location: locationPath + 'dist',
+      main: 'main'
+    }]
+  }, ['app']);
+
+})();
